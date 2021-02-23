6e9973dc2ad8555ce6610283d77966569e38caf0
Henrique Sousa <washington.sousa01@fatec.sp.gov.br>
2021-02-22 22:18:33 -0300

    Application base - Day 01

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Experiencebar } from './components/Experiencebar';
+import './styles/global.css';
+
+
+
+function App() {
+  return (
+    <div className="container">
+      <Experiencebar/>
+    </div>
+  );
+}
+
+export default App;
